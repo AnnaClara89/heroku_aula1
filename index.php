6f5ee3a0b3740ffab07afa60df8664aa36d22de7
AnnaClara89 <89108597+AnnaClara89@users.noreply.github.com>
2022-01-27 19:06:15 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php 
+
+?> 
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="index.css">
+    <meta charset="UTF-8"> <meta name="viewport" content="width-device-width, initial-scale=1.0">
+    <title>The Closet</title>
+</head>
+<body>
+    <div class="container">
+        <h2>Seja bem vindo(a) na The Closet! O que procura?</h2>
+        <form action="index.html" method="post">
+            <div class="input-field"> 
+            <input type="text" id="nome" placeholder="Digite seu nome">
+             <label for="nome">Peça:</label>
+        </div>
+
+        <div class="input-field"> 
+            <input type="text" id="nome" placeholder="Digite seu nome">
+             <label for="nome">Tamanho:</label>
+        </div>
+
+<div class="center"><
+    <button>Pesquisar</button>
+</div>
+</form>
+
+<div class="links">
+    <a href=""><p>Sobre a loja ?</p></a>
+    <a href=""><p>Parcerias</p></a>
+</div>
+</div>
+</body>
+</html>
